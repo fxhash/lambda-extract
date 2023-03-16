d9bb0752dbfc0022606c883dba4fefdfddf57343
louis <louisholley@me.com>
2023-03-16 11:49:01 +0000

    handle window.$fx undefined

--- index.js
+++ index.js
@@ -283,7 +283,7 @@ exports.handler = async (event, context) => {
       const extractedFeatures = await page.evaluate(
         () => {
           // v3 syntax
-          if (window.$fx._features) return JSON.stringify(window.$fx._features)
+          if (window.$fx?._features) return JSON.stringify(window.$fx._features)
           // deprecated syntax
           return JSON.stringify(window.$fxhashFeatures)
         }
